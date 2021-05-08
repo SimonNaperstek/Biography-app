05956f2ff1cdc533ed59a499a8a67d05e664fb03
simion <generations2023@gmail.com>
2021-05-08 17:02:28 +0500

    may work

--- routes/screen.js
+++ routes/screen.js
@@ -701,11 +701,12 @@ if(data){
     setTimeout(() => {
         let uid= uuid();
         console.log(uid);
+        doc.save('tmp/'+uid+'.pdf');
         // link
         let z = upload('tmp/'+uid+'.pdf', uid);
                 let link = "https://firebasestorage.googleapis.com/v0/b/" + 'generations-a0df0.appspot.com' + "/o/" + encodeURIComponent(uid) + "?alt=media&token=" + uid;
                 console.log(link);
-                doc.save('tmp/'+uid+'.pdf');
+                
         console.log('[+] saved!');
         res.send(link);
      
